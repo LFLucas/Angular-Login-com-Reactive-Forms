35e8891d6caeda9cf13e9deb02402e70ab72b144
Lucas Ferrari Lopes <lf.ferrarilucas@gmail.com>
2024-01-09 14:13:30 -0300

    falsa logica e layout do login prontos (começando resumo sobre angular reactive forms)

--- src/app/directives/track-status.directive.ts
+++ src/app/directives/track-status.directive.ts
@@ -3,18 +3,25 @@ import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
 @Directive({
   selector: '[rfTrackStatus]'
 })
+
 export class TrackStatusDirective {
 
   constructor(public renderer: Renderer2,
-              public elementRef: ElementRef) { }
-  
+              public elementRef: ElementRef) {  }
+            
   @HostListener("statusEvent", ["$event"])
-  toggleClass(event: string){
+  toggleClass(event: string){   
+    let timeoutID
 
-    //TODO - Inserir essa logica, o evento esta funcionando!
-  
+    if(event ==='success') this.renderer.removeClass(this.elementRef.nativeElement, 'error')
+    else this.renderer.removeClass(this.elementRef.nativeElement, 'success') 
+
+    this.renderer.addClass(this.elementRef.nativeElement, event)
+
+    clearTimeout(timeoutID)
+    timeoutID = setTimeout(() =>this.renderer.removeClass(this.elementRef.nativeElement, event) , 10000)
   }
-  
+
   
 
 }
